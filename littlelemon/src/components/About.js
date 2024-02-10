ca2f78eb95ef3075b31a0b5f9ae4f0bc41923f4a
Eric Tran <erictran0007@gmail.com>
2024-02-10 15:19:05 +1100

    Added About section

--- littlelemon/src/components/About.js
+++ littlelemon/src/components/About.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { Container, Link, Button, Text, Image, Heading, HStack, VStack, } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
-import Dishes from "../images/dishes.jpg"
+import Double from "../images/double.png"
 
 const title = "Little Lemon";
 const description = "In 1995, siblings Adrian and Mario established Little Lemon. Despite the city's diverse culinary scene, the brothers noticed a gap in Mediterranean cuisine in Chicago. Motivated to introduce the tastes of their Italian hometown to the city, they embarked on a mission to bring these flavors to the people. Almost three decades later, Adrian and Mario still actively manage the Little Lemon restaurant."
@@ -16,10 +16,10 @@ const About = () => {
   <FullScreenSection
     isDarkBackground
     backgroundColor="#495E57"
-    marginTop={"8rem"}
+    marginTop={"2rem"}
     marginLeft={"8rem"}
     marginRight={"8rem"}
-    marginBottom={"3rem"}
+    marginBottom={"2rem"}
 
   >
     <HStack>
@@ -42,36 +42,20 @@ const About = () => {
               href="./#book-section"
           >
 
-              <Button
-              marginTop="3rem"
-              colorScheme='yellow'
-              size="lg"
-              aria-label="On Click"
-              >{buttonText}</Button>
-
           </Link>
 
       </VStack>
 
        <Image
-        src={Dishes}
-        width="50%"
+        src={Double}
+        width="30%"
         margin="3rem"
         position="right"
 
         >
 
        </Image>
-       <Image
-        src={Dishes}
-        width="50%"
-        margin="3rem"
-        position="right"
-
-        >
-
-       </Image>
-
+     
        </HStack>
 
    
